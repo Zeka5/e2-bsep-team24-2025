23275fa9529df548b34b250190ae16e43c5d8a7b
Zeka5 <zekovicognjen753@gmail.com>
2025-09-22 13:52:58 +0200

    initial spring project

---
+++ bsep_backend/src/test/java/com/example/bsep_backend/BsepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bsep_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsepBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
